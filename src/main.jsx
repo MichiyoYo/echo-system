db294024c109aa1e8faf55368131d0b35d4f930c
Cristina Lester <cristina.deelester@gmail.com>
2023-08-19 08:56:50 -0700

    added modules and configs

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { CometChat } from '@cometchat-pro/chat';
+import { cometChat } from './app.config';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const appID = cometChat.APP_ID;
+const region = cometChat.APP_REGION;
+const appSetting = new CometChat.AppSettingsBuilder()
+  .subscribePresenceForAllUsers()
+  .setRegion(region)
+  .build();
+
+CometChat.init(appID, appSetting)
+  .then(() => {
+    ReactDOM.createRoot(document.getElementById('root')).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+    console.log('Initialization completed successfully');
+  })
+  .catch((error) => {
+    console.log('Initialization failed with error:', error);
+  });
